ee44fc049ce67dcfcfd8c0c6ec123662134b3b4c
michael <mmansourati@gmail.com>
2018-03-24 15:31:35 -0400

    fixed a few details in events page

--- src/pages/news_and_events.js
+++ src/pages/news_and_events.js
@@ -4,8 +4,8 @@ import '../styles/index.css'
 
 const News_and_events = () => (
   <div className="content-container">
+    <h2>News and Events</h2>
     <div className="event-item">
-      <h2>News and Events</h2>
       <h2 className="event-date">April 5th, 2018</h2>
       <h3>premiere of Pop Cycle for percussion quartet</h3>
       <span>McGill Percussion Ensemble concert</span>
