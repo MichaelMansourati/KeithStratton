1dd8024b81613471b56d1efcbe56518ea9005db7
michael <mmansourati@gmail.com>
2018-07-10 00:42:08 -0400

    prettier-ify JSON data

--- src/assets/corpus.js
+++ src/assets/corpus.js
@@ -1,147 +1,137 @@
-import afterTheYearOfGlad from "./audio/afterTheYearOfGlad.mp3"
-import ammophilaBreviligulata from "./audio/ammophilaBreviligulata.mp3"
-import anchor from "./audio/anchor.mp3"
-import duel from "./audio/duel.mp3"
-import empathyManoeuvre1 from "./audio/empathyManoeuvre1.mp3"
-import fiveMiniatures from "./audio/fiveMiniatures.mp3"
-import orchard from "./audio/orchard.mp3"
-import porcupine from "./audio/porcupine.mp3"
-import thisEmptyHouseIsOurBlankCanvas from "./audio/thisEmptyHouseIsOurBlankCanvas.mp3"
-import lullaby1 from "./audio/lullaby1.mp3"
+//make graphql query for all audio
 
-module.exports =
-{
+import afterTheYearOfGlad from './audio/afterTheYearOfGlad.mp3'
+import ammophilaBreviligulata from './audio/ammophilaBreviligulata.mp3'
+import anchor from './audio/anchor.mp3'
+import duel from './audio/duel.mp3'
+import empathyManoeuvre1 from './audio/empathyManoeuvre1.mp3'
+import fiveMiniatures from './audio/fiveMiniatures.mp3'
+import orchard from './audio/orchard.mp3'
+import porcupine from './audio/porcupine.mp3'
+import thisEmptyHouseIsOurBlankCanvas from './audio/thisEmptyHouseIsOurBlankCanvas.mp3'
+import lullaby1 from './audio/lullaby1.mp3'
+
+module.exports = {
   works: [
-  {
-    title: "Pop Cycle",
-    context: "composed for the McGill Percussion Ensemble",
-    ensemble: "percussion quartet",
-    duration: 15,
-    recording: "",
-    programNotes: "",
-    year: 2018,
-    shouldPrintYear: true
-  },
-  {
-    title: "Intoning on a Fleeced Vine",
-    context: "",
-    ensemble: "Piano trio with live electronics",
-    duration: 6,
-    recording: "",
-    programNotes: "",
-    year: 2017,
-    shouldPrintYear: true
-  },
-  {
-    title: "Empathy Manoeuvre",
-    context: "commissioned by Ear Camera",
-    ensemble: "clarinet, tenor sax, trombone, violin and percussion",
-    duration: 7,
-    recording: "",
-    programNotes: "",
-    year: 2017,
-    audio: empathyManoeuvre1
-  },
-  {
-    title: "Anchor",
-    context: "",
-    ensemble: "flute and electronic tape",
-    duration: 7,
-    recording: "",
-    programNotes: "",
-    year: 2017,
-    audio: anchor
-  },
-  {
-    title: "Orchard",
-    context: "",
-    ensemble: "piano and percussion",
-    duration: 7,
-    recording: "",
-    programNotes: "",
-    year: 2017,
-    audio: orchard
-  },
-  {
-    title: "Five Miniatures",
-    context: "",
-    ensemble: "mezzo-soprano, clarinet and piano",
-    duration: 6,
-    recording: "",
-    programNotes: "",
-    year: 2016,
-    shouldPrintYear: true,
-    audio: fiveMiniatures
-  },
-  {
-    title: "Duel",
-    context: "",
-    ensemble: "8-channel tape",
-    duration: 6,
-    recording: "",
-    programNotes: "",
-    year: 2016,
-    audio: duel
-  },
-  {
-    title: "Lullaby No. 1",
-    context: "",
-    ensemble: "harp, cello and vibraphone",
-    duration: 3,
-    recording: "",
-    programNotes: "",
-    year: 2016,
-    audio: lullaby1
-  },
-  {
-    title: "Kandinsky in Madras",
-    context: "",
-    ensemble: "string quartet",
-    duration: 7,
-    recording: "",
-    programNotes: "",
-    year: 2015,
-    shouldPrintYear: true
-  },
-  {
-    title: "After the Year of Glad",
-    context: "composed for the Acadia Wind Ensemble",
-    ensemble: "small chamber ensemble",
-    duration: 6,
-    recording: "",
-    programNotes: "",
-    year: 2015,
-    audio: afterTheYearOfGlad
-  },
-  {
-    title: "This Empty House is Our Blank Canvas",
-    context: "composed for the Acadia Saxophone Quartet",
-    ensemble: "saxophone quartet",
-    duration: 4,
-    recording: "",
-    programNotes: "",
-    year: 2015,
-    audio: thisEmptyHouseIsOurBlankCanvas
-  },
-  {
-    title: "Ammophila Breviligulata",
-    context: "composed for a workshop with the Ensemble Intercontemporain de Montréal (ECM+); premiered by the McGill Association of Student Composers (2016)",
-    ensemble: "flute, clarinet, violin, cello and piano",
-    duration: 4,
-    recording: "",
-    programNotes: "",
-    year: 2014,
-    shouldPrintYear: true,
-    audio: ammophilaBreviligulata
-  },
-  {
-    title: "Porcupine",
-    context: "composed for the Acadia Percussion Ensemble",
-    ensemble: "percussion sextet",
-    duration: 4,
-    recording: "",
-    programNotes: "",
-    year: 2014,
-    audio: porcupine
-  }
-]}
+    {
+      title: 'Pop Cycle',
+      context: 'composed for the McGill Percussion Ensemble',
+      ensemble: 'percussion quartet',
+      duration: 15,
+      programNotes: '',
+      year: 2018,
+      shouldPrintYear: true,
+    },
+    {
+      title: 'Intoning on a Fleeced Vine',
+      context: '',
+      ensemble: 'Piano trio with live electronics',
+      duration: 6,
+      programNotes: '',
+      year: 2017,
+      shouldPrintYear: true,
+    },
+    {
+      title: 'Empathy Manoeuvre',
+      context: 'commissioned by Ear Camera',
+      ensemble: 'clarinet, tenor sax, trombone, violin and percussion',
+      duration: 7,
+      programNotes: '',
+      year: 2017,
+      audio: empathyManoeuvre1,
+    },
+    {
+      title: 'Anchor',
+      context: '',
+      ensemble: 'flute and electronic tape',
+      duration: 7,
+      programNotes: '',
+      year: 2017,
+      audio: anchor,
+    },
+    {
+      title: 'Orchard',
+      context: '',
+      ensemble: 'piano and percussion',
+      duration: 7,
+      programNotes: '',
+      year: 2017,
+      audio: orchard,
+    },
+    {
+      title: 'Five Miniatures',
+      context: '',
+      ensemble: 'mezzo-soprano, clarinet and piano',
+      duration: 6,
+      programNotes: '',
+      year: 2016,
+      shouldPrintYear: true,
+      audio: fiveMiniatures,
+    },
+    {
+      title: 'Duel',
+      context: '',
+      ensemble: '8-channel tape',
+      duration: 6,
+      programNotes: '',
+      year: 2016,
+      audio: duel,
+    },
+    {
+      title: 'Lullaby No. 1',
+      context: '',
+      ensemble: 'harp, cello and vibraphone',
+      duration: 3,
+      programNotes: '',
+      year: 2016,
+      audio: lullaby1,
+    },
+    {
+      title: 'Kandinsky in Madras',
+      context: '',
+      ensemble: 'string quartet',
+      duration: 7,
+      programNotes: '',
+      year: 2015,
+      shouldPrintYear: true,
+    },
+    {
+      title: 'After the Year of Glad',
+      context: 'composed for the Acadia Wind Ensemble',
+      ensemble: 'small chamber ensemble',
+      duration: 6,
+      programNotes: '',
+      year: 2015,
+      audio: afterTheYearOfGlad,
+    },
+    {
+      title: 'This Empty House is Our Blank Canvas',
+      context: 'composed for the Acadia Saxophone Quartet',
+      ensemble: 'saxophone quartet',
+      duration: 4,
+      programNotes: '',
+      year: 2015,
+      audio: thisEmptyHouseIsOurBlankCanvas,
+    },
+    {
+      title: 'Ammophila Breviligulata',
+      context:
+        'composed for a workshop with the Ensemble Intercontemporain de Montréal (ECM+); premiered by the McGill Association of Student Composers (2016)',
+      ensemble: 'flute, clarinet, violin, cello and piano',
+      duration: 4,
+      programNotes: '',
+      year: 2014,
+      shouldPrintYear: true,
+      audio: ammophilaBreviligulata,
+    },
+    {
+      title: 'Porcupine',
+      context: 'composed for the Acadia Percussion Ensemble',
+      ensemble: 'percussion sextet',
+      duration: 4,
+      programNotes: '',
+      year: 2014,
+      audio: porcupine,
+    },
+  ],
+}
